d858665703927424506b70d178b6dd97d9c34e05
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-26 13:03:48 -0800

    add endpoint for POST request to add a customer

--- server/routes.js
+++ server/routes.js
@@ -2,5 +2,6 @@ const controller = require('./controllers');
 const router = require('express').Router();
 
 router.get('/customers/:id', controller.get.getOne);
+router.post('/customers', controller.post.addCustomer);
 
 module.exports = router;
