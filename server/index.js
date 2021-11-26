7fe6398ec3897d0e4409f470bc677a93ec93619c
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-26 12:00:15 -0800

    create server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const compression = require('compression');
+const morgan = require('morgan');
+
+app.use(express.json());
+
+app.use('/', router);
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
+/* ADD NODEMON SO SERVER WILL RESTART AUTOMATICALLY AFTER CHANGES */
