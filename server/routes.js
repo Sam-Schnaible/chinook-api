5ee4a5497d5f0207aaa344aea5c4f54896e9ea24
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-26 17:19:31 -0800

    add endpoint for PUT request to update a customer

--- server/routes.js
+++ server/routes.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 
 router.get('/customers/:id', controller.get.getOne);
 router.post('/customers', controller.post.addCustomer);
+router.put('/customers/:id', controller.put.updateCustomer);
 
 module.exports = router;
