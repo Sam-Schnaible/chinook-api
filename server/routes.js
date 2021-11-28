ead8cda63ef42025b308ddcc26bd0d4064e19ed6
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-27 16:47:33 -0800

    add endpoint for DELETE request to delete a customer

--- server/routes.js
+++ server/routes.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 router.get('/customers/:id', controller.get.getOne);
 router.post('/customers', controller.post.addCustomer);
 router.put('/customers/:id', controller.put.updateCustomer);
+router.delete('/customers/:id', controller.remove.removeCustomer);
 
 module.exports = router;
