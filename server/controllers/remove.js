07f11b5fb07be39dcbeaa2867f2a38f060cd44a3
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-27 16:55:55 -0800

    write request handler function for DELETE request; remove customer

---
+++ server/controllers/remove.js
@@ -0,0 +1,14 @@
+const { remove } = require('../models');
+
+module.exports = {
+  removeCustomer: async (req, res) => {
+    try {
+      let data = await remove.removeCustomer(req.params.id);
+      res.status(200).send(data);
+    } catch(err) {
+      console.log(err);
+      res.status(400).send(err);
+    }
+  },
+
+}
