17f8f72561f393b4a22ec562c917353e087747ab
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-29 14:44:49 -0800

    fix webpack config and node scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,25 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: "development",
+  entry: path.resolve(__dirname, './src/index.js'),
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+    ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
   output: {
-    path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
-    filename: "bundle.js"
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
   },
   devServer: {
-    static: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hot: true
+    static: path.resolve(__dirname, './public'),
+    historyApiFallback: true
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
