b3e091cb10a7e7a20ec8d5093d2e0c80d365b5a9
Sam Schnaible II <sschnaible83@gmail.com>
2021-11-29 19:29:00 -0800

    build delete component

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component} from "react";
+import Retrieve from './components/Retrieve.jsx';
+import Create from './components/Create.jsx';
+import DeleteCustomer from './components/DeleteCustomer.jsx';
+
+const App = () => {
+
+  return(
+    <div className="App">
+
+      <h1> Hello, World! </h1>
+      <Retrieve />
+      <Create />
+      <DeleteCustomer />
+    </div>
+  );
+}
+
+export default App;
--- server/controllers/remove.js
+++ server/controllers/remove.js
@@ -4,7 +4,7 @@ module.exports = {
   removeCustomer: async (req, res) => {
     try {
       let data = await remove.removeCustomer(req.params.id);
-      res.status(200).send(data);
+      res.status(200).send('Customer Successfully Deleted');
     } catch(err) {
       console.log(err);
       res.status(400).send(err.message);
