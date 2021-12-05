e6298546d997ea4d70a077f51fa472bdc05c92f1
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-05 15:10:41 -0800

    no change

--- server/models/remove.js
+++ server/models/remove.js
@@ -2,6 +2,7 @@ const db = require('../../database');
 
 module.exports = {
   removeCustomer: async id => {
+    // let result = await db.one(`SELECT * FROM customers WHERE id=${id}`)
     let result = await db.none(`
       DO $$
       DECLARE c_id integer[]= (SELECT ARRAY (SELECT id FROM invoices WHERE customer_id=${id} ));
