dd09ef269d1415ea4fc3f5dc91ff1733a21cb32a
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-18 13:42:24 -0800

    add validation rules and error handling to customers/:id route

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,8 @@
 const controller = require('./controllers');
 const router = require('express').Router();
+const { validate, getCustomerValidationRules } = require('./validateSanitize.js');
 
-router.get('/customers/:id', controller.get.getOne);
+router.get('/customers/:id', getCustomerValidationRules(), validate, controller.get.getOne);
 router.get('/customers', controller.get.getInitial);
 router.post('/customers', controller.post.addCustomer);
 router.put('/customers/:id', controller.put.updateCustomer);
