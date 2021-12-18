819383854e5ce1e339626a21540b1c5b8d7711d1
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-18 15:51:14 -0800

    parameterize delete query

--- server/models/remove.js
+++ server/models/remove.js
@@ -4,13 +4,13 @@ module.exports = {
   removeCustomer: async id => {
     let result = await db.none(`
     DO $$
-    DECLARE c_id integer[]= (SELECT ARRAY (SELECT id FROM invoices WHERE customer_id=${id} ));
+    DECLARE c_id integer[]= (SELECT ARRAY (SELECT id FROM invoices WHERE customer_id=$1 ));
     BEGIN
     DELETE FROM invoice_lines WHERE invoice_id=ANY (c_id);
-    DELETE FROM invoices WHERE customer_id=${id};
-    DELETE FROM customers WHERE id=${id};
+    DELETE FROM invoices WHERE customer_id=$1;
+    DELETE FROM customers WHERE id=$1;
     END $$;
-    `);
+    `, [id]);
     return 'Customer successfully deleted!'
   }
 }
