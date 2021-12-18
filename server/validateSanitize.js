ae109471c99b8673d02189360dadc2fad4c241db
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-18 13:20:05 -0800

    validate and sanitize get request for customer

--- server/validateSanitize.js
+++ server/validateSanitize.js
@@ -1,1 +1,19 @@
 const { check, body, validationResult } = require('express-validator');
+
+const getCustomerValidationRules = () => {
+  return [
+    check('id').matches('[0-9]').withMessage('ID must be an integer').trim().escape()
+  ]
+}
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+}
+
+module.exports = {
+  getCustomerValidationRules,
+  validate
+}
