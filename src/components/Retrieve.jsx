bd5a524edd697bdd875fa528dabc21dd7a21ccf7
Sam Schnaible II <sschnaible83@gmail.com>
2021-12-18 10:48:41 -0800

    remove bootstrap form

--- src/components/Retrieve.jsx
+++ src/components/Retrieve.jsx
@@ -49,34 +49,17 @@ const Retrieve = () => {
 
   return (
     <>
-      {/* <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1">
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form> */}
-      <form id="ret-form" onSubmit={(e) => handleOnSubmit(e)}>
-        <div class="mb-3">
-        <label class="form-label">
+      <h1>Retrieve Customer</h1>
+      <form className='form-style container-form' id='ret-form' onSubmit={(e) => handleOnSubmit(e)}>
+        <label>
           Enter Customer ID:
-          <input type='text' required class="form-control"
+          <input type='text' required
           value={customerInfo.id} onChange={(e) => setCustomerInfo({...customerInfo, id: e.target.value})}/>
         </label>
-        </div>
-        <input type='submit' value='Get Customer' class="btn btn-primary"
+        <input className='cursor' type='submit' value='Get Customer'
         onClick={() => setCustomerInfo({...customerInfo, method: 'get'})}
         />
-        <input type='submit' value='Delete Customer' class="btn btn-primary"
+        <input className='cursor' type='submit' value='Delete Customer'
         onClick={() => setCustomerInfo({...customerInfo, method: 'delete'})}
         />
         <p>ID Range: {rangeMin} - {rangeMax}</p>
